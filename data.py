feca2745f823485ece18df197028bb74959fafa9
jmartin-mchenry <jmartin3@students.mchenry.edu>
2026-08-26 11:29:48 -0500

    2b assignment

---
+++ data.py
@@ -0,0 +1,23 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[X] 1. Header Docstring included.
+[X] 2. Define a String variable.
+[X] 3. Define an Integer variable.
+[X] 4. Define a Float variable.
+[X] 5. Define a Boolean variable.
+[X] 6. Print all variables using F-Strings.
+[X] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+string_var = "this is a string!"
+int_var = 5
+float_var = 10.0
+bool_var = True
+
+print(f"string_var: {string_var}")
+print(f"int_var: {int_var}")
+print(f"float_var: {float_var}")
+print(f"bool_var: {bool_var}")
